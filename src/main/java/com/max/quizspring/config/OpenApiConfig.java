a6fd16b130e2736f5c8e38940e725cd3cc4a003b
Ajin007 <ajinroch007@gmail.com>
2024-03-24 17:13:18 +0530

    config to ensure the data is mapped perfectly

--- src/main/java/com/max/quizspring/config/OpenApiConfig.java
+++ src/main/java/com/max/quizspring/config/OpenApiConfig.java
@@ -6,12 +6,19 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(info = @Info(contact = @Contact(name = "Ajin Roch", email = "ajinroch007@gmail.com", url = "https://mwebsite"), title = "QuizSpring API", version = "1.0", description = "open Api document of the project", license = @License(name = "Liceence name", url = "url link")), servers = {
         @Server(description = "Local end setup", url = "http://locahost:8080"),
-        @Server(description = "Prod setup", url = "https://prod-api.com") })
+        @Server(description = "Prod setup", url = "https://prod-api.com") },
+
+        //added for the secrity scheme for the entire project, after this the lock icon wil appear in Swagger.html for 
+        // the entire project.
+        security = {
+            @SecurityRequirement(name = "bearerAuth")
+        })
 
 @SecurityScheme(name = "bearerAuth", description = "Authentication is done using JWT.", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER
 

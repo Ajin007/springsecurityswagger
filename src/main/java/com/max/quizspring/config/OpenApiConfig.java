d586544218bc121cf5def64ed19a65e9e1aed4e8
Ajin007 <ajinroch007@gmail.com>
2024-03-24 16:53:17 +0530

    Authorize icon will occur after this push

--- src/main/java/com/max/quizspring/config/OpenApiConfig.java
+++ src/main/java/com/max/quizspring/config/OpenApiConfig.java
@@ -1,34 +1,21 @@
 package com.max.quizspring.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(
-    info = @Info(
-        contact=@Contact(
-            name="Ajin Roch",
-            email =  "ajinroch007@gmail.com",
-            url = "https://mwebsite"),
-            title =  "QuizSpring API",
-            version = "1.0",
-            description="open Api document of the project",
-            license = @License(
-                name="Liceence name",
-                url="url link"
-            )),
-            servers ={
-                @Server(
-                    description="Local end setup",
-                    url="http://locahost:8080"
-                ),
-                @Server(
-                    description="Prod setup",
-                    url =  "https://prod-api.com"
-                )}
-                )
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Ajin Roch", email = "ajinroch007@gmail.com", url = "https://mwebsite"), title = "QuizSpring API", version = "1.0", description = "open Api document of the project", license = @License(name = "Liceence name", url = "url link")), servers = {
+        @Server(description = "Local end setup", url = "http://locahost:8080"),
+        @Server(description = "Prod setup", url = "https://prod-api.com") })
+
+@SecurityScheme(name = "bearerAuth", description = "Authentication is done using JWT.", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER
+
+)
 public class OpenApiConfig {
 
 }
